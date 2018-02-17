ab9f195fe956a842e3a767748c9b5cbe7f3bdec1
sd4483 <sudheer.paturi@hotmail.com>
2018-02-17 21:24:04 +0800

    Python Lessons
    Starting out

---
+++ myprogram.py
@@ -0,0 +1,12 @@
+# print("Hello")
+# print(7)
+# print(8)
+# print(10)
+# greeting = "Some Shit"
+# print(greeting)
+# greeting2 = input("Write Some Shit: ")
+# print(greeting2)
+# age = input("Enter your age: ")
+# new_age = int(age)+50
+# print(new_age)
+print(20-10/5*3**2) #Operation Rule: Exponent, Division, Multiplication, Addition, Subtraction
